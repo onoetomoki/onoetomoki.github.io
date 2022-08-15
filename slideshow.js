350959d3b6b7e1dd7c2ced4fc93fec28529ec600
onoetomoki <onoe.tomoki@gmail.com>
2022-08-15 21:15:58 +0900

    updated 22.8.15

--- slideshow.js
+++ slideshow.js
@@ -2,7 +2,7 @@ const img_src = ["image/hutami.jpg", "image/tikutetsu.jpg", "image/ema.jpg", "im
 let num = -1;
 
 function slide_time() {
-    if (num === 2) {
+    if (num === 3) {
     num = 0;
     } else {
     num++;
@@ -10,4 +10,4 @@ function slide_time() {
     document.getElementById("slide_img").src = img_src[num];
 }
 
-setInterval(slide_time, 1200);
+setInterval(slide_time, 2500);
