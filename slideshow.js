6d7fa6b14b00ee1ade52ca96a07ebb423d426aee
onoetomoki <onoe.tomoki@gmail.com>
2022-08-15 21:03:17 +0900

    updated 22.8.15

--- slideshow.js
+++ slideshow.js
@@ -1,33 +1,13 @@
-const picArray = [
-{src: 'images/hutami.jpg', title: 'Star'},
-{src: 'images/tikutetsu.jpg', title: 'Sakura'},
-{src: 'images/ema.jpg', title: 'Ema'},
-{src: 'images/hk.jpg', title: 'Hong Kong'}
-];
+const img_src = ["image/hutami.jpg", "image/tikutetsu.jpg", "image/ema.jpg", "image/hk.jpg"];
+let num = -1;
 
-let counter = 0;
-
-function changePicture() {
-  if (counter < picArray.length) {
-    document.getElementById('pics').src = picArray[counter].src;
-    document.getElementById('pic-title').innerHTML = picArray[counter].title;
-    counter++;
-  } else {
-    document.getElementById('pics').src = picArray[0].src;
-    document.getElementById('pic-title').innerHTML = picArray[0].title;
-    counter = 1;
-  } 
+function slide_time() {
+    if (num === 2) {
+    num = 0;
+    } else {
+    num++;
+    }
+    document.getElementById("slide_img").src = img_src[num];
 }
 
-let playingID = 0;
-
-function playSlidedeshow () {
-  if (playingID == 0) {
-    document.getElementById('playButton').innerHTML = 'STOP';
-    playingID = setInterval(changePicture, 2000);
-  } else {
-    document.getElementById('playButton').innerHTML = 'START';
-    clearInterval(playingID);
-    playingID = 0;
-  }
-}
+setInterval(slide_time, 1200);
